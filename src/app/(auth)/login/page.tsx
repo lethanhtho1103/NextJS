bb59ff427cb49080f3ea8998a44bed9c0be8ec11
Thanh Tho <letanhtho1953@gmail.com>
2024-07-26 18:45:45 +0700

    login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import LoginForm from "./login-form";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <>
+      <h1 className="text-xl font-semibold text-center">Đăng nhập</h1>
+      <div className="flex justify-center">
+        <LoginForm />
+      </div>
+    </>
+  );
 }
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -3,8 +3,11 @@ import RegisterForm from "./register-form";
 
 export default function Register() {
   return (
-    <div className="flex justify-center">
-      <RegisterForm />
-    </div>
+    <>
+      <h1 className="text-xl font-semibold text-center">Đăng ký</h1>
+      <div className="flex justify-center">
+        <RegisterForm />
+      </div>
+    </>
   );
 }
